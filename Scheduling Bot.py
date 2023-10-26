69fae824429baba90b05e52e28096044e48cab89
Muchatam <86578095+Muchatam@users.noreply.github.com>
2023-10-26 00:26:46 -0400

    Add files via upload

---
+++ Scheduling Bot.py
@@ -0,0 +1,63 @@
+import asyncio
+from datetime import datetime
+from playwright.async_api import async_playwright
+
+async def main():
+
+    # Get user information
+    username, password, sch_time = get_user_info()
+
+    # Launch Browser
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless = False, slow_mo=50)
+        context = await browser.new_context()
+        page_login = await context.new_page()
+        await page_login.goto("http://one.uf.edu")
+        async with page_login.expect_navigation():
+            await page_login.locator("button:has-text(\"Log in with GatorLink\")").click()
+        
+        # Username and Password Fill
+        await page_login.fill("input#username", username)
+        await page_login.fill("input#password", password)
+        async with page_login.expect_navigation():
+            await page_login.locator("button:has-text(\"Login\")").click()
+        await asyncio.sleep(10)
+
+        # Open Scheduling Pages
+        link = 'https://one.uf.edu/myschedule/2241'
+        course_numbers = [15755, 15713]
+        for i in range(len(course_numbers)):
+            asyncio.create_task(scheduling_tasks(context, link, course_numbers[i], sch_time))
+        
+        await asyncio.sleep(30)
+
+        await browser.close()
+
+
+async def scheduling_tasks(context, link, course_number, sch_time):
+    page_task = await context.new_page()
+    await page_task.goto(link)
+    
+    # Wait for Scheduled Time
+    current_time = datetime.now()
+    time_remaining = (sch_time - current_time).total_seconds()
+    await asyncio.sleep(time_remaining)
+    await page_task.goto(f'https://one.uf.edu/soc/registration-search/{link[-4:]}')
+
+    # Input Class Number
+    await page_task.fill("input#class-number", str(course_number))
+    async with page_task.expect_navigation():
+        await page_task.locator("button:has-text(\"Search\")").click()
+    
+    # Add Class
+    await page_task.locator("button:has-text(\"+ Add Class\")").click()
+    await page_task.get_by_role("button", name='Add').click()
+
+def get_user_info():
+    username = input("UF Username: ")
+    password = input("Password: ")
+    sch_time = datetime.strptime(input("Scheduling Time (Ex: 10/24/23 14:00:00): "), '%m/%d/%y %H:%M:%S')
+    return username, password, sch_time
+
+if __name__ == '__main__':
+    asyncio.run(main())
