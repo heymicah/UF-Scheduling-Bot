c333fced33a47f9557b7ede3b39fe389f29371cf
Muchatam <micahtam70@gmail.com>
2023-11-09 20:00:13 -0500

    Added more task statuses

--- Scheduling Bot.py
+++ Scheduling Bot.py
@@ -1,40 +1,39 @@
 import asyncio
-import json 
-import time
+import json
 from datetime import datetime
 from playwright.async_api import async_playwright
 
-async def main():
 
+async def main():
     # Get user information
     username, password, sch_time = get_user_info()
 
     # Launch Browser
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless = False, slow_mo=50)
+        browser = await p.chromium.launch(headless=False, slow_mo=50)
         context = await browser.new_context()
         page_login = await context.new_page()
         await page_login.goto("http://one.uf.edu")
         async with page_login.expect_navigation():
             await page_login.locator("button:has-text(\"Log in with GatorLink\")").click()
-        
+
         # Username and Password Fill
         await page_login.fill("input#username", username)
         await page_login.fill("input#password", password)
         async with page_login.expect_navigation():
             await page_login.locator("button:has-text(\"Login\")").click()
-        
+
         # Wait for login
         await asyncio.sleep(15)
 
         # Open Scheduling Pages
         link = 'https://one.uf.edu/myschedule/2241'
-        course_numbers = [15755, 15713]
+        course_numbers = [10718]
         count = []
         print('Launching Tasks ... ')
         for i in range(len(course_numbers)):
             asyncio.create_task(scheduling_tasks(context, link, course_numbers[i], sch_time, count, i))
-        
+
         while len(count) != len(course_numbers):
             await asyncio.sleep(2)
 
@@ -48,7 +47,7 @@ async def scheduling_tasks(context, link, course_number, sch_time, count, i):
         page_task = await context.new_page()
         print(f'[Task {i + 1}][{course_number}] Opening Link')
         await page_task.goto(link)
-        
+
         # Wait for Scheduled Time
         current_time = datetime.now()
         time_remaining = (sch_time - current_time).total_seconds()
@@ -62,22 +61,29 @@ async def scheduling_tasks(context, link, course_number, sch_time, count, i):
         await page_task.fill("input#class-number", str(course_number))
         async with page_task.expect_navigation():
             await page_task.locator("button:has-text(\"Search\")").click()
-        
+
         # Add Class
-        print(f'[Task {i + 1}][{course_number}] Registering for class')
         await page_task.locator("button:has-text(\"+ Add Class\")").click()
         await page_task.get_by_role("button", name='Add').click()
+        print(f'[Task {i + 1}][{course_number}] Registering for class')
+        await asyncio.sleep(7)
+        success_message = 'The following class was ADDED  successfully'
+        if await page_task.get_by_text('The following class was ADDED  successfully').text_content() == success_message:
+            print(f'[Task {i + 1}][{course_number}] Successfully Added Class')
+        else:
+            print(f'[Task {i + 1}][{course_number}] Error: Could not add class')
 
         # Return count
         count.append(1)
 
     except:
-        print(f'[Task {i+1}][{course_number}] Fatal Error')
+        print(f'[Task {i + 1}][{course_number}] Fatal Error')
         count.append(0)
+
 
 def get_user_info():
     try:
-        with open('userdata.txt') as f: 
+        with open('userdata.txt') as f:
             data = f.read()
             user_data = json.loads(data)
             username = user_data["Username"]
@@ -87,13 +93,14 @@ def get_user_info():
         data = {}
         data["Username"] = input("UF Username: ")
         data["Password"] = input("Password: ")
-        with open('userdata.txt', 'w') as f: 
+        with open('userdata.txt', 'w') as f:
             f.write(json.dumps(data))
         username = data["Username"]
         password = data["Password"]
-    
+
     sch_time = datetime.strptime(input("Scheduling Time (Ex: 10/24/23 14:00:00): "), '%m/%d/%y %H:%M:%S')
     return username, password, sch_time
+
 
 if __name__ == '__main__':
     asyncio.run(main())
